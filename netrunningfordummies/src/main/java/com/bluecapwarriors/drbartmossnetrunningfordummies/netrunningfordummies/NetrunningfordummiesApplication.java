09b600600d69787140209e9d66bcf18a486222d3
Isaiah <omegagamma314@gmail.com>
2024-08-16 20:50:43 -0700

    Spring Boot Setup

---
+++ netrunningfordummies/src/main/java/com/bluecapwarriors/drbartmossnetrunningfordummies/netrunningfordummies/NetrunningfordummiesApplication.java
@@ -0,0 +1,13 @@
+package com.bluecapwarriors.drbartmossnetrunningfordummies.netrunningfordummies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetrunningfordummiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetrunningfordummiesApplication.class, args);
+	}
+
+}
---
+++ netrunningfordummies/src/test/java/com/bluecapwarriors/drbartmossnetrunningfordummies/netrunningfordummies/NetrunningfordummiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecapwarriors.drbartmossnetrunningfordummies.netrunningfordummies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetrunningfordummiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
